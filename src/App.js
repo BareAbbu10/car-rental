2bab0f0a72dadbb693b5bb44519e48fdd335abbc
BareAbbu10 <infinity6box@gmail.com>
2023-09-18 23:41:43 +0500

    day-2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Navbar from './components/Navbar';
+import Home from './Pages/Home';
 import './Styles/App.css';
 import './Styles/Navbar.css';
+import './Styles/Hero.css';
 
 function App() {
-  return (
+  return (<>
     <Navbar/>
+    <Home/>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,11 @@ import React, { Component } from "react";
 export default class Navbar extends Component {
   render() {
     return (
-      <div className="Navbar container d-flex justify-content-between my-3">
+      <div className="Navbar container d-flex align-items-center justify-content-between my-3">
         <div>
           <h2>Logo</h2>
         </div>
-        <ul className="icons-navbar d-flex align-items-center">
+        <ul className="icons-navbar d-flex">
           <li className="fadeIn links-nav">Home</li>
           <li className="fadeIn links-nav">About</li>
           <li className="fadeIn links-nav">Vehicle</li>
